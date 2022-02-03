fbd1252ed5ed0f4459736a4983790a582c0ed52c
Kim-777 <iloo3012@naver.com>
2022-02-03 22:23:05 +0900

    Test routing in next

---
+++ pages/dogs.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Dogs: NextPage = () => {
+  return (
+    <div>
+      <p>dogs!</p>
+      <p>wal!!!</p>
+    </div>
+  );
+};
+
+export default Dogs;
