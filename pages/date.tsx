2f2c391d8c3ac50cf4eb211ce1e4f4ed01a99be4
Kim-777 <iloo3012@naver.com>
2022-02-17 22:50:19 +0900

    test date-fns lib

--- pages/date.tsx
+++ pages/date.tsx
@@ -2,11 +2,17 @@ import React, { FC } from "react";
 import Layout from "../components/layout";
 import dateStyles from "./date.module.css";
 import classNames from "classnames/bind";
+import { format, compareAsc, formatDistance } from "date-fns";
+import parseISO from "date-fns/parseISO";
+import { ko } from "date-fns/locale";
 
 const cx = classNames.bind(dateStyles);
 
-const Date: FC = () => {
+const DatePage: FC<any> = () => {
   const isTwo = true;
+  const date: Date = parseISO("1111");
+  // format(new Date(), "MM/dd/yyyy");
+  // const today = new Date();
 
   return (
     <Layout>
@@ -18,8 +24,13 @@ const Date: FC = () => {
       >
         Date
       </div>
+      <div>{date.toDateString()}</div>
+      <div>{format(new Date(), "MM/dd/yyyy")}</div>
+      <div>
+        {formatDistance(new Date(), new Date(2022, 0, 1), { locale: ko })}
+      </div>
     </Layout>
   );
 };
 
-export default Date;
+export default DatePage;
