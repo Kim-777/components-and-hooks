e0f6ef0b2fedcb866ee66996ed7d669793589fdd
Kim-777 <iloo3012@naver.com>
2022-06-12 23:13:19 +0900

    Setting antd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import "../styles/antd.less";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
@@ -9,6 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   console.log("app ::::::::");
   return (
     <>
+      <Script
+        strategy="beforeInteractive"
+        src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=ydg7e520nj"
+      />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
--- pages/code/index.tsx
+++ pages/code/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import classNames from "classnames/bind";
 import styles from "./code.module.css";
+import AntdMenu from "../../components/menu/AntdMenu";
 
 const CodesPage = () => {
-  return <div>CodesPage</div>;
+  return (
+    <div>
+      <AntdMenu />
+    </div>
+  );
 };
 
 export default CodesPage;
