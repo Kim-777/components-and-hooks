20dc9da32a45599a06f891a2c543622153bf2fc1
김예찬 <iloo3012@naver.com>
2022-06-15 14:12:21 +0900

    update _app to use react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,44 @@
+import React from "react";
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps, AppContext, AppInitialProps } from "next/app";
+import { Hydrate } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/antd.less";
+import { NextComponentType } from "next";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
+const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
+  Component,
+  pageProps,
+}) => {
+  const qyeryClientRef = React.useRef<QueryClient>();
+
+  if (!qyeryClientRef.current) {
+    qyeryClientRef.current = new QueryClient();
+  }
 
   console.log("app ::::::::");
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+    <QueryClientProvider client={qyeryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
     </QueryClientProvider>
   );
-}
+};
+
+MyApp.getInitialProps = async ({
+  Component,
+  ctx,
+}: AppContext): Promise<AppInitialProps> => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps };
+};
 
 export default MyApp;
