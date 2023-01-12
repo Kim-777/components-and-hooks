bce34138140e2a9a14e9381602b63c5873d1fe75
Kim-777 <iloo3012@naver.com>
2023-01-12 10:29:38 +0900

    Update: dependencies version

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,44 +1,28 @@
+import dayjs from "dayjs";
 import { useRouter } from "next/router";
 import React from "react";
 import { useQuery } from "react-query";
+import weekDay from "dayjs/plugin/weekday";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import isoWeek from "dayjs/plugin/isoWeek";
+
+dayjs.extend(weekDay);
+dayjs.extend(weekOfYear);
+dayjs.extend(isoWeek);
 
 const Test = () => {
-  const router = useRouter();
-  const testQuery = useQuery(
-    ["test", "only"],
-    (): Promise<string> =>
-      new Promise((resolve) => {
-        setTimeout(() => {
-          console.log("데이터 패칭패칭 패칭!!");
-          resolve("데이터 패칭 완료!");
-        }, 5000);
-      })
-  );
-  console.log("router.query ::::::: ", router.query);
+  const nowAgo = dayjs("2022.1.1");
+  const twentyMonthAgo = dayjs("2021.12.24");
+  console.log("twentyMonthAgo ::: ", twentyMonthAgo.week());
+  console.log("nowAgo :::: ", nowAgo.week());
 
-  // React.useEffect(() => {
-  //   console.log('router.query ::::::: ' )
-  // }, [])
-
-  if (testQuery.isLoading) {
-    return <div>로딩 중.........</div>;
-  }
-
-  if (testQuery.isFetching) {
-    return <div>패칭 중.........</div>;
-  }
-
+  const now = dayjs("2023.1.1");
+  const twentyMonth = dayjs("2022.12.31");
+  console.log("twentyMonth ::: ", twentyMonth.week());
+  console.log("now :::: ", now.week());
   return (
     <div>
-      <div>Test</div>
-      <div>{testQuery.data}</div>
-      <button
-        onClick={() => {
-          testQuery.refetch();
-        }}
-      >
-        refetching
-      </button>
+      <div>날짜 테스트</div>
     </div>
   );
 };
